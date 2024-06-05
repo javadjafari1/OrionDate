0736748e72aaf111ea5b1cce7a135a7613d8717c
javadjafari <javad2147@yahoo.com>
2024-06-05 22:04:49 +0330

    update jvmTarget to 21

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
         publishLibraryVariants("release")
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                JavaVersion.VERSION_21.toString()
             }
         }
     }
